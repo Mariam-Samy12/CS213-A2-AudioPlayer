84dd1271a31989aa1f7350cb049a887cc10ea2f9
Mariam_Samy12 <2006mariamsamy@gmail.com>
2025-10-22 23:20:03 +0300

    Add feature 4

--- Source/playerGUI.h
+++ Source/playerGUI.h
@@ -10,7 +10,7 @@ public:
 	~PlayerGUI() override;
 
 	void resized() override;
-	
+
 	void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
 	void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
 	void releaseResources();
@@ -18,10 +18,12 @@ public:
 private:
 	PlayerAudio playerAudio;
 	// GUI elements
-	juce::TextButton loadButton{ "Load File"};
-	juce::TextButton restartButton{ "Restart"};
-	juce::TextButton stopButton{ "Stop"};
+	juce::TextButton loadButton{ "Load File" };
+	juce::TextButton restartButton{ "Restart" };
+	juce::TextButton stopButton{ "Stop" };
 	juce::Slider volumeSlider;
+	juce::TextButton loopButton{ "Loop: Off" };//LOOP
+	
 	std::unique_ptr < juce::FileChooser> fileChooser;
 	// Event handlers
 	void buttonClicked(juce::Button* button) override;
